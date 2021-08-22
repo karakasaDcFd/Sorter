bbafb52779c62943ef236e8fcbd8df6dfdafea3e
KarakasaDcFd <recieveout1224@gmail.com>
2021-08-23 01:54:33 +0900

    Implement Sorter.pm

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,30 @@
 # implement this
+package Sorter;
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    $self->set_values;
+    return $self;
+}
+
+sub set_values {
+    my ($self, @args) = @_;
+    $self->{values} = \@args;
+}
+
+sub get_values {
+    my $self = shift;
+    return @{ $self->{values} };
+}
+
+sub sort {
+    my $self = shift;
+    my @sorted = sort { $a <=> $b } @{ $self->{values} };
+    $self->set_values(@sorted);
+}
+
+1;
